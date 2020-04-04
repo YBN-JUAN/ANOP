54bc94334784affff0c3c24454ed7feebf3fc037
ZYF <m13110683916@163.com>
2020-04-04 09:26:37 +0800

    初始化后端项目结构

---
+++ backend/src/test/java/edu/fzu/anop/AnopApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fzu.anop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
