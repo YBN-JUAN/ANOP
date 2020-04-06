6eba504c15dee5e0f085415278644a143bac6ae6
koananan <yyx13055737361@163.com>
2020-04-06 02:01:36 -0400

    修改app路由配置

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,9 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import {WelcomeComponent} from './pages/welcome/welcome.component';
 import {NotificationComponent} from './pages/notification/notification.component';
 import {MemorandumComponent} from './pages/memorandum/memorandum.component';
+import {LoginComponent} from './pages/welcome/login/login.component';
+import {RegisterComponent} from './pages/welcome/register/register.component';
 
 const routes: Routes = [
-  { path: 'welcome/login', component: WelcomeComponent },
+  {
+    path: 'welcome', component: WelcomeComponent,
+    children:[
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: '**', component: LoginComponent },
+      { path: '', component:LoginComponent }
+    ]
+  },
   { path: 'notification', component: NotificationComponent },
   { path: 'memorandum', component: MemorandumComponent},
   { path: '', redirectTo: '/welcome/login', pathMatch: 'full'},
--- frontend/src/app/pages/welcome/welcome-routing.module.ts
+++ frontend/src/app/pages/welcome/welcome-routing.module.ts
@@ -1,36 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {WelcomeComponent} from './welcome.component';
-import {LoginComponent} from './login/login.component';
-import {RegisterComponent} from './register/register.component';
 
 
 const routes: Routes = [
-  { path: 'welcome',
-    component: WelcomeComponent,
-    children: [
-      {
-        path: 'login',
-        component: LoginComponent,
-        pathMatch: 'full'
-      },
-      {
-        path: 'register',
-        component: RegisterComponent,
-        pathMatch: 'full'
-      },
-      {
-        path: '',
-        redirectTo: '/login',
-        pathMatch: 'full'
-      },
-      {
-        path: '**',
-        redirectTo: '/login',
-        pathMatch: 'full'
-      }
-    ]
-  }
 ];
 
 @NgModule({
