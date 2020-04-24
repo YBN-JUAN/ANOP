702cdca7aa79f39ab23425520496b4691e9e56a8
ZYF <m13110683916@163.com>
2020-04-24 09:56:53 +0800

    修复由图片上传配置引起的swagger文档无法访问的bug

--- backend/src/main/java/edu/fzu/anop/config/FileHandleConfig.java
+++ backend/src/main/java/edu/fzu/anop/config/FileHandleConfig.java
@@ -12,6 +12,10 @@ public class FileHandleConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations(
+                "classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
     }
 
     public static String getUploadPath() {
