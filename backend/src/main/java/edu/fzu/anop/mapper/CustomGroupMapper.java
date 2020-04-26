eb3257532b6f822ebb809f3012acee0cfd011aa5
Shen Zhifeng <shen_zhifeng@outlook.com>
2020-04-27 01:36:57 +0800

    完善自定义mapper注释

--- backend/src/main/java/edu/fzu/anop/mapper/CustomGroupMapper.java
+++ backend/src/main/java/edu/fzu/anop/mapper/CustomGroupMapper.java
@@ -4,10 +4,34 @@ import edu.fzu.anop.resource.GroupResource;
 
 import java.util.List;
 
+/**
+ * 自定义通知群组Mapper
+ *
+ * @author Xue_Feng
+ */
 public interface CustomGroupMapper {
+    /**
+     * 选取指定主键的通知
+     *
+     * @param groupId 主键
+     * @return 通知
+     */
     GroupResource selectGroupByPrimary(Integer groupId);
 
+    /**
+     * 获取指定用户创建的通知群组列表
+     *
+     * @param userId 用户id
+     * @return 通知列表
+     */
     List<GroupResource> listUserCreateGroup(Integer userId);
 
+    /**
+     * 获取指定用户管理的通知群组列表
+     *
+     * @param userId
+     * @param isAdmin
+     * @return
+     */
     List<GroupResource> listUserGroup(Integer userId, Byte isAdmin);
 }
