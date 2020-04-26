d365ef37518206bc8a01ae4c4024d15163f1a2d1
koananan <yyx13055737361@163.com>
2020-04-26 06:15:38 -0400

    完成群组详情数据读取

--- frontend/src/app/pages/notification/subscription-center/group-detail/group-detail.component.ts
+++ frontend/src/app/pages/notification/subscription-center/group-detail/group-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Group} from '../../../../share/model/group-info';
+import {SubscriptionCenterService} from '../../../../share/service/subscription-center.service';
 
 @Component({
   selector: 'app-group-detail',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./group-detail.component.css']
 })
 export class GroupDetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public group: Group;
+  constructor(private route: ActivatedRoute,
+              private service: SubscriptionCenterService) {
   }
 
+  ngOnInit(): void {
+    this.group = new Group();
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.group.remark = "";
+    this.group.id = id;
+    this.group.title = "...";
+    this.group.creationDate = "...";
+    this.group.avatarUrl = "...";
+    this.getGroupInfo(id);
+  }
+
+  getGroupInfo(id:number) {
+    this.service.getGroup(id).subscribe(
+      data => {
+        this.group = data;
+        console.log(data)
+        console.log(this.group);
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
 }
