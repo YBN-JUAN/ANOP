31699db2ff316f93bdf279a0375fbe3d055db5bd
highkickk <60636752+highkickk@users.noreply.github.com>
2020-04-26 19:22:29 +0800

    修改个人中心bug

--- frontend/src/app/pages/user-center/edit-profile/edit-profile.component.ts
+++ frontend/src/app/pages/user-center/edit-profile/edit-profile.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserCenterService } from '../../../share/service/user-center.service';
 import { UserInfo } from '../../../share/model/user-info';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-edit-profile',
@@ -12,8 +11,7 @@ export class EditProfileComponent implements OnInit {
   public user:UserInfo;
 
   constructor(
-    public service:UserCenterService,
-    public http:HttpClient
+    public service:UserCenterService
   ) { }
 
   ngOnInit(): void {
--- frontend/src/app/share/service/user-center.service.ts
+++ frontend/src/app/share/service/user-center.service.ts
@@ -1,25 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { UserInfo } from '../model/user-info';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserCenterService {
-  public url: string = 'http://localhost:8080/v1/profile';
+  public url: string = 'http://localhost:8080/v1/';
   storageOk: boolean = false;
 
   constructor(public http:HttpClient) {
   }
 
   getConfig(){
-    return this.http.get<UserInfo>(this.url);
+    return this.http.get<UserInfo>(this.url + "profile");
   }
 
   storageUser() {
     if (this.storageOk) { return; }
-    this.http.get<UserInfo>(this.url).subscribe(data => (
+    this.http.get<UserInfo>(this.url + "profile").subscribe(data => (
       localStorage.setItem('userid', String(data.id)),
         localStorage.setItem('username', String(data.userName))
     ))
@@ -32,10 +31,21 @@ export class UserCenterService {
         'Content-Type': 'application/json'
       })
     };
-    this.http.put(this.url, {nickname:nickName}, httpOptions)
+    this.http.put(this.url + "profile", {nickname:nickName}, httpOptions)
       .subscribe(response => {
         console.log(response);
-      }
-    );
+      });
+  }
+
+  resetPassword(newPassword: string, oldPassword: string) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    this.http.post(this.url + "account/password",{newPassword:newPassword, oldPassword:oldPassword}, httpOptions)
+      .subscribe(response => {
+        console.log(response);
+      });
   }
 }
