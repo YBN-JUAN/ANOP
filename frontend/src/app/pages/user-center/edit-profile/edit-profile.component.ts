2e68d553e50b06f3d819f44dfd8ffcb6a3d2a543
highkickk <60636752+highkickk@users.noreply.github.com>
2020-04-30 23:43:39 +0800

    URL头像上传

--- frontend/src/app/pages/user-center/edit-profile/edit-profile.component.ts
+++ frontend/src/app/pages/user-center/edit-profile/edit-profile.component.ts
@@ -8,14 +8,25 @@ import { UserInfo } from '../../../share/model/user-info';
   styleUrls: ['./edit-profile.component.css']
 })
 export class EditProfileComponent implements OnInit {
-  public user:UserInfo;
+  public user: UserInfo = {
+    id: 0,
+    email: '',
+    userName: '',
+    nickName: '',
+    creationTime: '',
+    avatarUrl: '',
+  };
+  public avatarURL = '';
 
   constructor(
-    public service:UserCenterService
+    public service:UserCenterService,
   ) { }
 
   ngOnInit(): void {
-    this.service.getConfig().subscribe(data => this.user = data);
+    this.service.getConfig().subscribe(data => {
+      this.user = data;
+      this.user.avatarUrl = data.avatarUrl;
+    });
   }
 
   onEditUsername() {
@@ -26,6 +37,17 @@ export class EditProfileComponent implements OnInit {
   onSubmit() {
     document.getElementById('hide').style.display='none';
     document.getElementById('show').style.display='block';
-    this.service.updateUserInfo(this.user.nickName);
+    this.service.updateUserInfo(this.user.nickName, this.user.avatarUrl);
+  }
+
+  onUrlUpload() {
+    document.getElementById('upload-show').style.display='none';
+    document.getElementById('upload-hide').style.display='block';
+  }
+
+  getAvatarUrl() {
+    document.getElementById('upload-hide').style.display='none';
+    document.getElementById('upload-show').style.display='block';
+    this.service.updateUserInfo(this.user.nickName, this.avatarURL);
   }
 }
