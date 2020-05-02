1639c51064b373baf70f87da09b3cccfe2c306fe
koananan <yyx13055737361@163.com>
2020-05-02 04:07:11 -0400

    完成注册模块的功能

--- frontend/src/app/pages/welcome/register/register.component.ts
+++ frontend/src/app/pages/welcome/register/register.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {HttpClient} from '@angular/common/http';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
-import {AuthService} from '../../../share/service/auth.service';
-import {UserCenterService} from '../../../share/service/user-center.service';
+import {Email, RegisterInfo} from '../../../share/model/register-info';
+import {RegisterService} from '../../../share/service/register.service';
 
 @Component({
   selector: 'app-register',
@@ -13,26 +12,73 @@ import {UserCenterService} from '../../../share/service/user-center.service';
 export class RegisterComponent implements OnInit {
   validateForm: FormGroup;
   error: boolean = false;
-  errMsg:string;
+  errorMsg: string = "";
 
   constructor(private fb: FormBuilder,
-              private http:HttpClient,
-              private router: Router,
-              private app: AuthService,
-              private userCenterService: UserCenterService) {}
+              private service: RegisterService,
+              private route: Router) {}
 
   ngOnInit(): void {
     this.validateForm = this.fb.group({
       username: [null, [Validators.required]],
-      email: [null, [Validators.required]],
+      email: [null, [Validators.email, Validators.required]],
       code: [null, [Validators.required]],
       password: [null, [Validators.required]],
+      confirm: [null, [this.confirmValidator]]
     });
   }
+
+  confirmValidator = (control: FormControl): { [s: string]: boolean } => {
+    if (!control.value) {
+      return { error: true, required: true };
+    } else if (control.value !== this.validateForm.controls.password.value) {
+      return { confirm: true, error: true };
+    }
+    return {};
+  };
 
   submitForm(): void {
     if (!this.validateForm.valid) {
       this.openDirtyControl(this.validateForm);
+    } else {
+      let info = new RegisterInfo();
+      info.code = this.validateForm.controls["code"].value;
+      info.email = this.validateForm.controls["email"].value;
+      info.password = this.validateForm.controls["password"].value;
+      info.username = this.validateForm.controls["username"].value;
+      //console.log(info);
+      this.service.register(info).subscribe(
+        data => {
+          this.error = false;
+          this.route.navigateByUrl("/welcome/login");
+        },
+        error => {
+          this.error = true;
+          this.errorMsg = "注册失败";
+          console.log(error);
+        }
+      )
+    }
+  }
+
+  getCode() {
+    let body = new Email();
+    if (this.validateForm.controls["email"].valid) {
+      body.email = this.validateForm.controls["email"].value;
+      this.service.sendCode(body).subscribe(
+        data => {
+          this.error = false;
+          window.alert("验证码已经发送");
+        },
+        error => {
+          this.error = true;
+          this.errorMsg = "验证码发送失败";
+          console.log(error);
+        }
+      )
+    } else {
+      this.error = true;
+      this.errorMsg = "邮箱地址不合法";
     }
   }
 
