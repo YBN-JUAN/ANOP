0314e856cca7a26bf19359096417c881ef15ce88
YBN <493670735@qq.com>
2020-05-05 00:42:32 +0800

    实现显示所有订阅群组的通知的页面（暂时不能显示阅读状态）。侧边栏订阅中心修改为子菜单风格，添加查找群组、所有通知等紫菜单项。

--- frontend/src/app/pages/notification/subscription-center/group-message/group-message.component.ts
+++ frontend/src/app/pages/notification/subscription-center/group-message/group-message.component.ts
@@ -1,8 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {toNumber} from "ng-zorro-antd";
-import {ActivatedRoute} from "@angular/router";
-import {GroupMessageService} from "./group-message.service";
-import {GroupInfoModel} from "../../../../share/model/group-info.model";
+import {ActivatedRoute} from '@angular/router';
+import {GroupMessageService} from './group-message.service';
+import {GroupInfoModel} from '../../../../share/model/group-info.model';
+import {ResponseModel} from '../../../../share/model/response.model';
+import {NotificationInfoModel} from '../../../../share/model/notification-info.model';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-group',
@@ -10,30 +12,44 @@ import {GroupInfoModel} from "../../../../share/model/group-info.model";
   styleUrls: ['./group-message.component.css']
 })
 export class GroupMessageComponent implements OnInit {
-  public gid;
+
+  loading = true;
+
+  notificationList: NotificationInfoModel[] = [];
 
   constructor(private route: ActivatedRoute, private service: GroupMessageService) {
   }
 
   ngOnInit(): void {
+    this.getAllMessages();
+    this.loading = false;
   }
 
-  doGet() {
-    const gid = toNumber(this.route.snapshot.paramMap.get('gid'));
-    this.service.getGroupMessage(this.gid).subscribe(
-      (data) => {
-        console.log(data)
-      }
-    )
-  }
+  // doGet() {
+  //   const gid = toNumber(this.route.snapshot.paramMap.get('gid'));
+  //   this.service.getGroupMessage(this.gid).subscribe(
+  //     (data) => {
+  //       console.log(data)
+  //     }
+  //   )
+  // }
 
   getAllMessages() {
     this.service.getSubscribeGroups().subscribe(
-      (groups: GroupInfoModel[]) => {
-        for (const group in groups) {
-
+      (responseModel: ResponseModel<GroupInfoModel>) => {
+        for (const group of responseModel.list) {
+          this.service.getGroupMessage(group.id).subscribe(
+            (response: ResponseModel<NotificationInfoModel>) => {
+              this.notificationList = this.notificationList.concat(response.list);
+              console.log(this.notificationList)
+            }, (error: HttpErrorResponse) => {
+              console.log(error);
+            }
+          );
         }
+      }, error => {
+        console.log(error);
       }
-    )
+    );
   }
 }
