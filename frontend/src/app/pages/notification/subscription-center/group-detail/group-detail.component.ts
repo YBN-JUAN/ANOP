bfaddb4d40503d22a7bcae4a1d72bbf1b08d3543
YBN <493670735@qq.com>
2020-05-06 00:47:01 +0800

    group-detail.component.ts：修复单引号问题。
    Signed-off-by: YBN <493670735@qq.com>

--- frontend/src/app/pages/notification/subscription-center/group-detail/group-detail.component.ts
+++ frontend/src/app/pages/notification/subscription-center/group-detail/group-detail.component.ts
@@ -29,11 +29,11 @@ export class GroupDetailComponent implements OnInit {
   ngOnInit(): void {
     this.group = new GroupInfoModel();
     const id = toNumber(this.route.snapshot.paramMap.get('id'));
-    this.group.remark = "";
+    this.group.remark = '';
     this.group.id = id;
-    this.group.title = "...";
-    this.group.creationDate = "...";
-    this.group.avatarUrl = "...";
+    this.group.title = '...';
+    this.group.creationDate = '...';
+    this.group.avatarUrl = '...';
     this.getGroupInfo(id);
     this.loadDataFromServer(this.pageIndex, this.pageSize);
   }
@@ -54,7 +54,7 @@ export class GroupDetailComponent implements OnInit {
   quitGroup() {
     this.modal.confirm({
       nzTitle: '你确定要退出这个群组吗?',
-      //nzContent: '<b style="color: red;">Some descriptions</b>',
+      // nzContent: '<b style="color: red;">Some descriptions</b>',
       nzOkText: '确定',
       nzOkType: 'danger',
       nzOnOk: () => {
@@ -71,14 +71,16 @@ export class GroupDetailComponent implements OnInit {
     pageSize: number,
   ): void {
     this.loading = true;
-    this.pubService.getGroupUser(this.group.id,"id", pageIndex, pageSize).subscribe(data => {
+    this.pubService.getGroupUser(this.group.id, 'id', pageIndex, pageSize).subscribe(data => {
         this.loading = false;
         this.total = 200; // mock the total data here
         this.listOfUsers = data.list;
         console.log(this.listOfUsers);
         console.log(data);
       },
-      error => { console.log(error); }
+      error => {
+        console.log(error);
+      }
     );
   }
 
