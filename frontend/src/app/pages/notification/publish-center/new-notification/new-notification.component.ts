76bd257a3ff3007281c430bbbea673fe31ca17d7
YBN <493670735@qq.com>
2020-05-06 01:01:38 +0800

    实现发布通知功能。
    Signed-off-by: YBN <493670735@qq.com>

--- frontend/src/app/pages/notification/publish-center/new-notification/new-notification.component.ts
+++ frontend/src/app/pages/notification/publish-center/new-notification/new-notification.component.ts
@@ -1,4 +1,9 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {NotificationModel} from '../../../../share/model/notification.model';
+import {NewNotificationService} from './new-notification.service';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-new-notification',
@@ -7,11 +12,44 @@ import {Component, OnInit} from '@angular/core';
 })
 export class NewNotificationComponent implements OnInit {
 
-  constructor() {
+  newNotificationForm: FormGroup;
+  private gid;
 
+  constructor(private route: ActivatedRoute, private formBuilder: FormBuilder, private service: NewNotificationService) {
+    this.newNotificationForm = this.formBuilder.group({
+      title: ['', [Validators.required]],
+      content: ['', [Validators.required]],
+    });
   }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      this.gid = params.gid;
+    });
+  }
 
+  resetForm(e: MouseEvent): void {
+    e.preventDefault();
+    this.newNotificationForm.reset();
+    // tslint:disable-next-line:forin
+    for (const key in this.newNotificationForm.controls) {
+      this.newNotificationForm.controls[key].markAsPristine();
+      this.newNotificationForm.controls[key].updateValueAndValidity();
+    }
+  }
+
+  submitFrom() {
+    const title = this.newNotificationForm.controls.title.value;
+    const content = this.newNotificationForm.controls.content.value;
+
+    this.service.sendNotification(new NotificationModel(title, content, this.gid)).subscribe(
+      (response) => {
+        console.log(response);
+        alert('发布成功。');
+      }, (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    );
+    console.log(new NotificationModel(title, content, this.gid));
   }
 }
