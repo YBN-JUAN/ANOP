6bf8287eb1f9a0b02fecdc3674363ccae5c419f3
Shen Zhifeng <shen_zhifeng@outlook.com>
2020-05-07 00:30:43 +0800

    修复注册失败问题

--- frontend/src/app/share/model/register-info.model.ts
+++ frontend/src/app/share/model/register-info.model.ts
@@ -1,30 +1,8 @@
-export class RegisterInfoModel {
-  constructor() {
-  }
-
-  private _code: string;
-
-  set code(value: string) {
-    this._code = value;
-  }
-
-  private _email: string;
-
-  set email(value: string) {
-    this._email = value;
-  }
-
-  private _password: string;
-
-  set password(value: string) {
-    this._password = value;
-  }
-
-  private _username: string;
-
-  set username(value: string) {
-    this._username = value;
-  }
+export class RegisterInfo {
+  code: string;
+  email: string;
+  password: string;
+  username: string;
 }
 
 export class Email {
