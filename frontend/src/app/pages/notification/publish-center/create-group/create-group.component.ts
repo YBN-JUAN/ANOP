0aa1c97e9a92f39837bced4846b9a83228dcd121
YBN <493670735@qq.com>
2020-05-06 23:12:27 +0800

    完成创建群组模块的基本布局和表单提交事件。
    Signed-off-by: YBN <493670735@qq.com>

--- frontend/src/app/pages/notification/publish-center/create-group/create-group.component.ts
+++ frontend/src/app/pages/notification/publish-center/create-group/create-group.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {NewNotificationService} from '../new-notification/new-notification.service';
 
 @Component({
   selector: 'app-create-group',
@@ -6,11 +8,33 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./create-group.component.css']
 })
 export class CreateGroupComponent implements OnInit {
+  createGroupForm: FormGroup;
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder, private service: NewNotificationService) {
+    this.createGroupForm = this.formBuilder.group({
+      title: ['', [Validators.required]],
+      remark: ['', null],
+      permission: ['', [Validators.required]]
+    });
   }
 
   ngOnInit(): void {
   }
 
+  resetForm(e: MouseEvent): void {
+    e.preventDefault();
+    this.createGroupForm.reset();
+    // tslint:disable-next-line:forin
+    for (const key in this.createGroupForm.controls) {
+      this.createGroupForm.controls[key].markAsPristine();
+      this.createGroupForm.controls[key].updateValueAndValidity();
+    }
+  }
+
+  submitFrom() {
+    const title = this.createGroupForm.controls.title.value;
+    const remark = this.createGroupForm.controls.remark.value;
+    const permission = this.createGroupForm.controls.permission.value;
+    console.log(permission)
+  }
 }
