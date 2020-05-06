76e2a38f181d3e7e0a29c5505a5b21fbf33212c2
YBN <493670735@qq.com>
2020-05-06 23:42:44 +0800

    完成创建群组模块，新建用来保存接口URL静态资源的类。
    Signed-off-by: YBN <493670735@qq.com>

--- frontend/src/app/pages/notification/publish-center/create-group/create-group.service.ts
+++ frontend/src/app/pages/notification/publish-center/create-group/create-group.service.ts
@@ -1,10 +1,17 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {ApiUrlResource} from '../../../../share/resource/api-url.resource';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CreateGroupService {
 
-  constructor() {
+  constructor(private http: HttpClient) {
+  }
+
+  createGroupPost(title: string, remark, permission) {
+    const url = ApiUrlResource.publishGroupUrl;
+    return this.http.post(`${url}`, {title, remark, permission});
   }
 }
---
+++ frontend/src/app/share/resource/api-url.resource.ts
@@ -0,0 +1,3 @@
+export class ApiUrlResource {
+  static publishGroupUrl = 'http://localhost:8080/v1/pub/groups';
+}
