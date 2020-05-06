202f227e4b7466ff39802ad1d375b621c864b54a
YBN <493670735@qq.com>
2020-05-06 23:42:44 +0800

    完成创建群组模块，新建用来保存接口URL静态资源的类。
    Signed-off-by: YBN <493670735@qq.com>

--- frontend/src/app/share/resource/api-url.resource.ts
+++ frontend/src/app/share/resource/api-url.resource.ts
@@ -1,11 +1,3 @@
 export class ApiUrlResource {
-  static publishGroupUrl = 'http://localhost:8080/v1/pub/groups'
-  static publishRequestUrl = 'http://localhost:8080/v1/pub/requests'
-  static subscribeGroupUrl = 'http://localhost:8080/v1/sub/groups'
-  static categoryUrl = 'http://localhost:8080/v1/categories'
-  static todoUrl = 'http://localhost:8080/v1/todos'
-  static signUpUrl = 'http://localhost:8080/v1/signup'
-  static profileUrl = 'http://localhost:8080/v1/profile'
-  static avatarUrl = 'http://localhost:8080/v1/avatar'
-  static userUrl = 'http://localhost:8080/v1/user'
+  static publishGroupUrl = 'http://localhost:8080/v1/pub/groups';
 }
