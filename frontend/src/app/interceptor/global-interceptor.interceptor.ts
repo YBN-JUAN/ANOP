26848d43c03da62d8ebc63994c5aa416f82eb217
YBN <493670735@qq.com>
2020-05-30 00:08:08 +0800

    fix：未登录状态下直接输入路由访问模块时，提示未登录并自动导航到登录页。
    Signed-off-by: YBN <493670735@qq.com>

--- frontend/src/app/interceptor/global-interceptor.interceptor.ts
+++ frontend/src/app/interceptor/global-interceptor.interceptor.ts
@@ -1,28 +1,30 @@
-import { Injectable } from '@angular/core';
-import { CookieService } from 'ngx-cookie-service';
+import {Injectable} from '@angular/core';
+import {CookieService} from 'ngx-cookie-service';
 import {
-  HttpRequest,
-  HttpHandler,
+  HttpErrorResponse,
   HttpEvent,
+  HttpHandler,
   HttpInterceptor,
-  HttpResponse,
-  HttpErrorResponse
+  HttpRequest,
+  HttpResponse
 } from '@angular/common/http';
-import { Observable, of, throwError } from 'rxjs';
-import { mergeMap, catchError } from 'rxjs/operators';
+import {Observable, of, throwError} from 'rxjs';
+import {catchError, mergeMap} from 'rxjs/operators';
 import {Router} from '@angular/router';
+import {NzMessageService} from 'ng-zorro-antd';
 
 @Injectable()
 export class GlobalInterceptor implements HttpInterceptor {
 
-  constructor(private cookieService:CookieService,private route: Router) {}
+  constructor(private cookieService: CookieService, private route: Router, private msg: NzMessageService) {
+  }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const xhr = request.clone({
       headers: request.headers
         .set('X-Requested-With', 'XMLHttpRequest')
-        .set('X-XSRF-TOKEN',this.cookieService.get('XSRF-TOKEN')),
-      withCredentials:true
+        .set('X-XSRF-TOKEN', this.cookieService.get('XSRF-TOKEN')),
+      withCredentials: true
 
     });
     return next.handle(xhr).pipe(mergeMap((event: any) => {
@@ -32,9 +34,12 @@ export class GlobalInterceptor implements HttpInterceptor {
   }
 
   private handleData(event: HttpResponse<any> | HttpErrorResponse): Observable<any> {
-    if(event.status===401 && !this.route.routerState.snapshot.url.startsWith('/welcome')){
+    if ((event.status === 401 || event.status === 403) && !this.route.routerState.snapshot.url.startsWith('/welcome')) {
       console.log(this.route);
-      this.route.navigateByUrl('/welcome/login');
+      this.route.navigateByUrl('/welcome/login').then(() => {
+        // alert('未登录')
+        this.msg.error('未登录')
+      });
     }
     return throwError(event);
   }
