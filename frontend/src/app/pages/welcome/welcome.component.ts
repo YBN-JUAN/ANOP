a2feb069196d55155df2b93f6164b7c611308c6e
YBN <493670735@qq.com>
2020-05-29 23:07:47 +0800

    refactor：排除一些代码风格上的小问题。
    Signed-off-by: YBN <493670735@qq.com>

--- frontend/src/app/pages/welcome/welcome.component.ts
+++ frontend/src/app/pages/welcome/welcome.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
-import {stringify} from 'querystring';
 
 @Component({
   selector: 'app-welcome',
@@ -27,22 +26,26 @@ export class WelcomeComponent implements OnInit {
       title: ''
     }
   ];
-  constructor(private router:Router) {}
+
+  constructor(private router: Router) {
+  }
 
   ngOnInit(): void {
     this.initTab();
   }
 
   taoTo(tab) {
-    this.router.navigateByUrl(`/welcome/${tab.key}`);
+    this.router.navigateByUrl(`/welcome/${tab.key}`).then(
+      () => {
+      });
   }
 
   initTab() {
     // 设置再次刷新页面时还是显示之前的tab
     const key = this.router.url.substr(this.router.url.lastIndexOf('/') + 1);
-    const idx = this.tabs.findIndex(w => w.key === key);
-    this.selectIndex = idx;
-    this.router.navigateByUrl(`/cardWhole/${this.tabs[this.selectIndex].key}`);
+    this.selectIndex = this.tabs.findIndex(w => w.key === key);
+    this.router.navigateByUrl(`/cardWhole/${this.tabs[this.selectIndex].key}`).then(() => {
+    });
   }
 
 }
