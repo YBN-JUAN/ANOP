8644a557b9728c774f5edef253f0dfb08e9470f7
YBN <493670735@qq.com>
2020-06-03 00:12:02 +0800

    manage-group.component：初步搭建群组管理页面。
    Signed-off-by: YBN <493670735@qq.com>

--- frontend/src/app/pages/notification/publish-center/manage-group/manage-group.component.ts
+++ frontend/src/app/pages/notification/publish-center/manage-group/manage-group.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {GroupUpdateInfo, UpdateUserInfo} from '../../../../share/model/group-info.model';
 import {GroupUser} from '../../../../share/model/user-info.model';
 import {ActivatedRoute, Router} from '@angular/router';
 import {PublishCenterService} from '../../../../share/service/publish-center.service';
-import {NzTableQueryParams, toNumber} from 'ng-zorro-antd';
-import {error} from 'selenium-webdriver';
+import {NzModalService, NzTableQueryParams, toNumber} from 'ng-zorro-antd';
+import {TableParamsModel} from '../../../../share/model/table-params.model';
+import {NotificationInfoModel} from '../../../../share/model/notification-info.model';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-manage-group',
@@ -12,19 +14,41 @@ import {error} from 'selenium-webdriver';
   styleUrls: ['./manage-group.component.css']
 })
 export class ManageGroupComponent implements OnInit {
+
+  pageSize = 6;
   updateGroupInfo: GroupUpdateInfo;
   groupId: number;
   permission: string;
   total = 1;
   listOfUsers: GroupUser[] = [];
   loading = true;
-  pageSize = 3;
-	pageIndex = 1;
+  expandSet = new Set<number>();
+  pageIndex = 1;
   adminInfo: UpdateUserInfo;
+  mTable: TableParamsModel<GroupUser> = new TableParamsModel(true, 6, 1);
+  nTable: TableParamsModel<NotificationInfoModel> = new TableParamsModel(true, 6, 1);
+  visible = false;
 
   constructor(private route: ActivatedRoute,
               private service: PublishCenterService,
-              private router: Router) {
+              private router: Router,
+              private modal: NzModalService) {
+  }
+
+  loadNotificationData(pageIndex: number, pageSize: number): void {
+    this.nTable.loading = true;
+    this.service.getGroupNotifications(this.groupId, 'creationDate desc', pageIndex, pageSize).subscribe(
+      data => {
+        this.nTable.loading = false;
+        this.nTable.total = data.total;
+        this.nTable.data = data.list;
+        console.log(this.nTable.data);
+      },
+      (response: HttpErrorResponse) => {
+        this.service.msg.error(response.error.message)
+        console.log(response);
+      }
+    );
   }
 
   ngOnInit(): void {
@@ -37,7 +61,7 @@ export class ManageGroupComponent implements OnInit {
     this.getGroupInfo(id);
     this.loadDataFromServer(this.pageIndex, this.pageSize);
   }
-	
+
 	getGroupInfo(id: number) {
     this.service.getGroup(id).subscribe(
       data => {
@@ -86,32 +110,80 @@ export class ManageGroupComponent implements OnInit {
   }
 
   updateUser(info: GroupUser) {
-    if (info.isAdmin == 0) {
+    if (info.isAdmin === 0) {
       this.adminInfo.isAdmin = 1;
     } else {
       this.adminInfo.isAdmin = 0;
     }
-    let uid = info.userId;
+    const uid = info.userId;
     this.service.updateGroupUser(this.groupId, uid, this.adminInfo).subscribe(
-     data => {
-       console.log("update user to admin ok");
-       this.loadDataFromServer(this.pageIndex, this.pageSize);
-     } ,
+      () => {
+        console.log('update user to admin ok');
+        this.loadDataFromServer(this.pageIndex, this.pageSize);
+      },
       error => {
-       console.log(error);
+        console.log(error);
       }
     );
   }
 
-  deletUser(uid: number) {
-    this.service.delGroupUser(this.groupId, uid).subscribe(
-      data => {
-        console.log("del user ok");
+  deleteUser(uid: number) {
+    this.service.removeGroupUser(this.groupId, uid).subscribe(
+      () => {
+        console.log('del user ok');
         this.loadDataFromServer(this.pageIndex, this.pageSize);
       },
       error => {
         console.log(error);
       }
     )
+  }
+
+  dismissGroup() {
+    this.modal.confirm({
+      nzTitle: '您确定要解散这个群组吗?',
+      // nzContent: '<b style="color: red;">Some descriptions</b>',
+      nzOkText: '确定',
+      nzOkType: 'danger',
+      nzOnOk: () => {
+        this.service.dismissGroup(this.groupId).subscribe(
+          () => {
+            this.service.msg.success('您已解散该群');
+          },
+          error => {
+            console.log('delete group fail', error);
+          }
+        );
+        this.ngOnInit();
+      },
+      nzCancelText: '取消',
+      nzOnCancel: () => console.log('Cancel')
+    });
+  }
+
+  onNotificationQueryParamsChange(params: NzTableQueryParams): void {
+    console.log(params);
+    const {pageSize, pageIndex} = params;
+    this.loadNotificationData(pageIndex, pageSize);
+  }
+
+  openDrawer(): void {
+    this.visible = true;
+  }
+
+  closeDrawer(): void {
+    this.visible = false;
+  }
+
+  onExpandChange(id: number, checked: boolean): void {
+    if (checked) {
+      this.expandSet.add(id);
+    } else {
+      this.expandSet.delete(id);
+    }
+  }
+
+  deleteNotification(id: number) {
+
   }
 }
